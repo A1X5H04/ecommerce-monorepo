ae3f93251124773dc80d5652ca7cbabdd4b82ece
Alish Baig <alishbaig2004@gmail.com>
2025-11-09 10:47:46 +0530

    feat: new 'admin-cms' app

---
+++ apps/admin-cms/app/page.tsx
@@ -0,0 +1,13 @@
+// import { Button } from "@/components/ui/button";
+import { Container, Button } from "@chakra-ui/react";
+import { } from "@emotion/react"
+
+export default function Home() {
+  return (
+    <Container>
+      <Button className="">
+        This is a goddamn button
+      </Button>
+    </Container>
+  );
+}
