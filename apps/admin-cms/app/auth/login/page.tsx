521ba5e75d97367cdf68f9d19765cd7a52f710a1
Alish Baig <alishbaig2004@gmail.com>
2025-11-11 23:00:20 +0530

    feat: add auth layout and design login page

--- apps/admin-cms/app/auth/login/page.tsx
+++ apps/admin-cms/app/auth/login/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Box, Center, Heading, Image, Input, Stack, Text } from '@chakra-ui/react'
+import NextImage from "next/image"
+
+import { Field } from '@/components/ui/field'
+import { PasswordInput } from '@/components/ui/password-input'
+import { Button } from '@/components/ui/button'
 
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+
+    <Center height="full" width="full">
+      <Box mx="auto" width="full" maxWidth="xl">
+        <Image asChild mb="24">
+          <NextImage src="/logo.svg" alt="Logo Image" width={150} height={150} />
+        </Image>
+
+      <Stack gapY={0} marginBottom="10">
+        <Heading size="2xl" fontWeight="medium">
+          Login To Dashboard
+        </Heading>
+        <Text color="fg.muted">
+          Fill the below form to login.
+        </Text>
+      </Stack>
+      <Stack marginBottom="10">
+        <Field label="Email Address">
+          <Input type="email" placeholder='sharukhkhan@example.com' />
+        </Field>
+        <Field label="Password">
+          <PasswordInput placeholder='sharukhkhan_chaddi_me_ghan' />
+        </Field>
+      </Stack>
+        <Button width="full" type="submit" fontWeight="semibold">
+          Login
+        </Button>
+      </Box>
+    </Center>
   )
 }
 
